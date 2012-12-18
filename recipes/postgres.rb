f2b347d8787aefcd5a25fe155829bd155026aafa
Emanuele Zattin <emanuelez@gmail.com>
2012-12-18 17:36:16 +0100

    Fix locales

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -20,6 +20,14 @@
  
 include_recipe "postgresql::server"
 
+execute :fix_locale do
+  command 'export LANGUAGE=en_US.UTF-8'
+  command 'export LANG=en_US.UTF-8'
+  command 'export LC_ALL=en_US.UTF-8'
+  command 'locale-gen en_US.UTF-8'
+  command 'sudo dpkg-reconfigure locales'
+end
+
 pg_user node['gerrit']['database']['username'] do
   privileges :superuser => false, :createdb => false, :login => true
   password node['gerrit']['database']['password']
